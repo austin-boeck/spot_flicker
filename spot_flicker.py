50ea7a5a0c950a20563cc6610dd348bb7068a34d
austin-boeck <austin.boeck@gmail.com>
2016-02-09 18:09:47 -0800

    A cursory update
    Not entirely completed, can't work out how to highlight specific areas
    on the plot

--- spot_flicker.py
+++ spot_flicker.py
@@ -1,5 +1,8 @@
 import numpy as np
+import matplotlib
+from matplotlib.patches import Polygon
 import matplotlib.pyplot as plt
+
 #
 
 '''
@@ -10,12 +13,32 @@ file = '4171937.lc'
 
 time, flux, err = np.loadtxt(file, usecols=(1,2,3), unpack = True)
 
-#plot of time vs flux
+
+
+#plot of time vs flux of an interesting light curve segment
 
 plt.figure()
 plt.plot(time, flux, 'g')
-#plt.ylabel('Equivalent Duration (ergs maybe?)')
-#plt.xlabel('Flare Duration (Minutes)')
+plt.xlim(774, 786)
+plt.ylim(13750,14000)
+plt.ylabel('Equivalent Duration (ergs maybe?)')
+plt.xlabel('Time (Days)')
 plt.show()
+plt.savefig('CandidateTime' )
 
+#Here I attempt to do the same as above, but with flare durations highlighted
+
+file2 = '4171937.dat.fbeye'
+ 
+flaretimes = np.loadtxt(file2, usecols=(5,6))
+
+#plt.figure()
+#plt.plot(time, flux, 'g')
+#plt.Polygon(flaretimes, closed=False , c = 'y')
+#plt.xlim(774, 786)
+#plt.ylim(13750,14000)
+#plt.ylabel('Equivalent Duration (ergs maybe?)')
+#plt.xlabel('Time (Days)')
+#plt.show()
+#plt.savefig('CT_W_flares' )
 
